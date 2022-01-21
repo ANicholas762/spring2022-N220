c9c4b6edb9f87e345768e2c72bf96a3ddbef49aa
ANicholas762 <84145964+ANicholas762@users.noreply.github.com>
2022-01-21 13:33:40 -0500

    personal comp update

---
+++ Labs/practiceLabs/js/app.js
@@ -0,0 +1,11 @@
+function setup() {
+    createCanvas(480, 120);
+}
+
+function draw() {
+    background(204);
+    point(240, 60);
+    point(20,50);
+    point(420, 110);
+    line(20, 50, 420, 110);
+}
---
+++ personalComposition/js/app.js
@@ -0,0 +1,24 @@
+circ = 100; //varibles for the circle and square
+sq = 100;
+
+function setup() {          //canvas size and background color
+    createCanvas(800, 600);
+    background(255, 191, 15);
+}
+
+function draw() {                   // the position and size of the circle as well as the color
+    circle(mouseX, mouseY, circ);
+    fill(34, 76, 130);
+    
+
+    square(400, 300, sq);   // the position and size of the square as well as the color
+    fill(69, 105, 31)
+
+    
+    if(mouseIsPressed) {        //if statement that declares that if the mouse is pressed, the circle will increase in size by 2
+        circ = circ + 2;
+    }
+
+
+
+}
