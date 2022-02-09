93d7cd577ca65cc644e8591a46daa6d5642aba2e
ANicholas762 <84145964+ANicholas762@users.noreply.github.com>
2022-02-09 00:11:34 -0500

    lecture lab

---
+++ Labs/lecture2/js/app.js
@@ -0,0 +1,6 @@
+let dvTest = document.getElementById("dvTest");
+
+dvTest.innerHTML = "Hello World!"; 
+dvTest.style.backgroundColor = "#990099";
+dvTest.style.height = "50px";
+dvTest.style.fontSize = "40px";
